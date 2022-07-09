6b240a7a20dcbb9860b4007fe670c044cbcd67cb
reinaldncku <57290344+reinaldncku@users.noreply.github.com>
2022-07-09 17:15:46 +0800

    Add files via upload

---
+++ Car_example1.py
@@ -0,0 +1,13 @@
+class Car:
+	def __init__(self, c, n, s, f):
+		self.color = c
+		self.name = n
+		self.speed = s
+		self.fuel = f
+
+car1 = Car("blue", "May ToyoSiya", 0, 1000)
+car2 = Car("red", "Subarururu", 100, 5)
+
+print (car1.name)
+print (car2.name)
+
