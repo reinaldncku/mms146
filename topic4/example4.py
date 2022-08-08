3e21fb47a7d4cab2a4fc25df14893c979078aa25
reinaldncku <57290344+reinaldncku@users.noreply.github.com>
2022-08-08 19:32:44 +0800

    Create example4.py

---
+++ topic4/example4.py
@@ -0,0 +1,22 @@
+class HumanBeing:
+	def __init__(self, n, w, c):
+		self.__name = n
+		self.weight = w
+		self.citizenship = c
+
+	def set_name(self, n):
+		self.__name = n
+
+	def get_name(self):
+		print (self.__name)
+		self.__get_more_name()
+		
+	def __get_more_name(self):
+	    print (self.__name)
+	    print (self.__name)
+	   
+# Main Program
+iw = HumanBeing("Ing-wen", 70, "Taiwanese")
+iw.get_name()
+iw.__get_more_name()
+
