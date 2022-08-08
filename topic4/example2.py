bb1edfe8cb1487cd4ad3bb492a43d848fdf3be5e
reinaldncku <57290344+reinaldncku@users.noreply.github.com>
2022-08-08 19:28:51 +0800

    Create example2.py

---
+++ topic4/example2.py
@@ -0,0 +1,18 @@
+
+class HumanBeing:
+	def __init__(self, n, w, c):
+		self.name = n
+		self.weight = w
+		self.citizenship = c
+	   
+# Main Program
+iw = HumanBeing("Ing-wen", 70, "Taiwanese")
+print (iw.name)
+
+
+
+
+
+
+
+
