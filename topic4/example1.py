d33f99400304cd377dcfa7418fe83d730a5ec24e
reinaldncku <57290344+reinaldncku@users.noreply.github.com>
2022-08-08 19:28:30 +0800

    Create example1.py

---
+++ topic4/example1.py
@@ -0,0 +1,19 @@
+
+class HumanBeing:
+	def __init__(self, n, w, c):
+		self.__name = n
+		self.weight = w
+		self.citizenship = c
+	   
+# Main Program
+iw = HumanBeing("Ing-wen", 70, "Taiwanese")
+# print (iw.__name)  #This line of code will produce an error
+# print (iw.name)    #Also, this line will result in an error too
+
+
+
+
+
+
+
+
