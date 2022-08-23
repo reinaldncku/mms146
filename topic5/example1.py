5892f3bae9ec785d13ca0394d13da5b1d130914c
reinaldncku <57290344+reinaldncku@users.noreply.github.com>
2022-08-23 14:14:02 +0800

    Create example1.py

---
+++ topic5/example1.py
@@ -0,0 +1,23 @@
+class Animal: 
+  	def print_age(self): 
+    		print("Age of the animal.") 
+     
+class Rabbit(Animal): 
+  	def print_age(self): 
+    		print("Age of rabbit.") 
+       
+class Horse(Animal): 
+  	def print_age(self): 
+    		print("Age of horse.") 
+
+class Ostrich (Animal):
+	  def fly(self):
+		  print ("I am flying")
+
+a1 = Rabbit()
+a2 = Horse()
+a3 = Ostrich()
+
+a1.print_age()
+a2.print_age()
+a3.print_age()
